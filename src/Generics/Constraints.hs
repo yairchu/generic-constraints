b896e876b5e4325da23eec553113c66aad90a395
Yair Chuchem <yairchu@gmail.com>
2019-08-28 17:15:10 +0300

    Simplify

--- src/Generics/Constraints.hs
+++ src/Generics/Constraints.hs
@@ -1,18 +1,26 @@
 {-# LANGUAGE
     PolyKinds
   , TypeFamilies
+  , TypeOperators
   , ConstraintKinds
-  , FlexibleInstances
-  , MultiParamTypeClasses
-  , UndecidableSuperClasses
   #-}
 module Generics.Constraints (Constraints) where
 
-import Data.Kind (Constraint)
-import qualified Generics.Constraints.Internal as I
+import GHC.Generics
+import GHC.Types (Constraint)
 
--- | Constraint-level 'duplicate', of kind @(k -> Constraint) -> k -> k -> Constraint@.
-class (c a, a ~ b) => D (c :: k -> Constraint) a b
-instance (c a, a ~ b) => D c a b
+type family Constraints' (t :: * -> *) (c :: * -> Constraint) :: Constraint
+type instance Constraints' V1 c = ()
+type instance Constraints' U1 c = ()
+type instance Constraints' (f :+: g) c = (Constraints' f c, Constraints' g c)
+type instance Constraints' (f :*: g) c = (Constraints' f c, Constraints' g c)
+type instance Constraints' (f :.: g) c = Constraints' g c
+type instance Constraints' Par1 c = ()
+type instance Constraints' (Rec1 f) c = ()
+type instance Constraints' (K1 i a) c = c a
+type instance Constraints' (M1 i t f) c = Constraints' f c
 
-type Constraints t c = I.Constraints t t (D c)
+-- | `Constraints` is a constraint type synonym, containing the constraint
+-- requirements for an instance for `t` of class `c`.
+-- It requires an instance of class `c` for each component of `t`.
+type Constraints t c = Constraints' (Rep t) c
