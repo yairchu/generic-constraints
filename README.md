69dae7a639c2083cbe818b10ebd8cb51e0b2153d
Yair Chuchem <yairchu@gmail.com>
2019-08-29 09:47:32 +0300

    Documentation

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-Constraints via Generic
+# Standalone deriving without boiler-plate
 
 Write short and concise contexts based on generics.
+
+Instead of writing boiler-plate standalone deriving clauses in the form of
+
+```Haskell
+deriving instance [Various Eq Constraints Here] => Instance Eq MyType
+```
+
+With generic-constraints you can use
+
+```Haskell
+deriving instance Constraints MyType Eq => Eq MyType
+```
+
+Or, using TH, simply
+
+```Haskell
+makeDeriving ''Eq ''MyType
+```
+
+And for several classes and types:
+
+```Haskell
+makeDerivings [''Eq, ''Ord, ''Show] [''MyType, ''MyOtherType]
+```
+
+## Credits
+
+This library was extracted from the [one-liner](http://hackage.haskell.org/package/one-liner) library by Sjoerd Visscher and Xia Li-yao.
--- src/Generics/Constraints.hs
+++ src/Generics/Constraints.hs
@@ -1,3 +1,16 @@
+-- | Write short and concise contexts based on generics.
+--
+-- Instead of writing boiler-plate standalone deriving clauses in the form of
+--
+-- > deriving instance [Various Eq Constraints Here] => Instance Eq MyType
+--
+-- You can use
+--
+-- > deriving instance Constraints MyType Eq => Eq MyType
+--
+-- And with the `TemplateHaskell` helpers @makeDeriving@ and others,
+-- your declarations can be even more concise.
+
 {-# LANGUAGE
     PolyKinds
   , TypeFamilies
@@ -7,6 +20,7 @@
   #-}
 module Generics.Constraints
     ( Constraints
+      -- | Template Haskell helpers
     , makeDeriving, makeDerivings
     , makeInstance, makeInstances
     ) where
@@ -30,20 +44,48 @@ type instance Constraints' (M1 i t f) c = Constraints' f c
 -- | `Constraints` is a constraint type synonym, containing the constraint
 -- requirements for an instance for `t` of class `c`.
 -- It requires an instance of class `c` for each component of `t`.
+--
+-- Useful for the writing the context of `StandaloneDeriving` clauses:
+--
+-- > deriving instance Constraints MyType Eq => Eq MyType
 type Constraints t c = Constraints' (Rep t) c
 
+-- | `TemplateHaskell` generation of multiple standalone deriving declarations.
+--
+-- > makeDerivings [''Eq, ''Ord, ''Show] [''MyType, ''MyOtherType]
+--
+-- Generates the deriving declarations for all given classes and types.
 makeDerivings :: [T.Name] -> [T.Name] -> T.DecsQ
 makeDerivings = makeMany makeDeriving
 
+-- | `TemplateHaskell` generation of multiple instance declarations.
+--
+-- > makeInstances [''Binary, ''NFData] [''MyType, ''MyOtherType]
+--
+-- Generates the instances declarations for all given classes and types.
 makeInstances :: [T.Name] -> [T.Name] -> T.DecsQ
 makeInstances = makeMany makeInstance
 
 makeMany :: (T.Name -> T.Name -> T.DecsQ) -> [T.Name] -> [T.Name] -> T.DecsQ
 makeMany f classes types = concat <$> sequence (f <$> classes <*> types)
 
+-- | `TemplateHaskell` generation of a standalone deriving declaration.
+--
+-- > makeDeriving ''Ord ''MyType
+--
+-- Generates:
+--
+-- > deriving instance Constraints MyType Ord => Ord MyType
 makeDeriving :: T.Name -> T.Name -> T.DecsQ
 makeDeriving = makeCommon (T.StandaloneDerivD Nothing)
 
+-- | `TemplateHaskell` generation of an instance declaration with no methods.
+--
+-- > makeDeriving ''Binary ''MyType
+--
+-- Generates:
+--
+-- > instance Constraints MyType Binary => Binary MyType
 makeInstance :: T.Name -> T.Name -> T.DecsQ
 makeInstance = makeCommon (\c i -> T.InstanceD Nothing c i [])
 
